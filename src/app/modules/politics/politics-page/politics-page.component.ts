dec1b17782fca010589a1d274b19207148e8034a
Anastasiia Santalova <anastasiia_santalova@epam.com>
2020-09-07 21:32:40 +0300

    [NP-008] Add table component

--- src/app/modules/politics/politics-page/politics-page.component.ts
+++ src/app/modules/politics/politics-page/politics-page.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./politics-page.component.css']
 })
 export class PoliticsPageComponent implements OnInit {
+  data = [
+    { name: 'Steve Jobs', raiting: '51%', job: 'manager' },
+    { name: 'Bon Jovi', raiting: '35%', job: 'singer' },
+    { name: 'Michael Kors', raiting: '12%', job: 'designer' },
+    { name: 'Other', raiting: '2%', job: '-' },
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'job', label: 'Job' },
+    { key: 'raiting', label: 'Rating' },
+  ];
 
   constructor() { }
 
--- src/app/modules/politics/politics.module.ts
+++ src/app/modules/politics/politics.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PoliticsRoutingModule } from './politics-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { PoliticsPageComponent } from './politics-page/politics-page.component';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
   declarations: [
     PoliticsPageComponent,
+    TableComponent,
   ],
   imports: [
     CommonModule,
     PoliticsRoutingModule,
+    SharedModule,
   ],
   exports: [],
 })
